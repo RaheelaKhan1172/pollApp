bd05b2aa7faa2622dbd1fc8335a959752df2b092
Raheela Khan <raheelak20@gmail.com>
2016-02-09 23:56:27 -0800

    necessary components set up

--- config/express.js
+++ config/express.js
@@ -1,2 +1,13 @@
-app.set('views','./app/views');
-app.set('view engine','ejs');
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override');
+
+module.exports = function() {
+  var app = express();
+  app.use(bodyParser.urlencoded({extended:true}));
+  app.set('views','./app/views');
+  app.set('view engine','ejs');
+  require('../app/routes/index.server.routes.js')(app);
+  app.use(express.static('./public'));
+  return app;
+}
